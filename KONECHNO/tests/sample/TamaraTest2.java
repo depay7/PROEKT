bfa714034e3ea80ea239e9d08e915dfacd314768
Tamara Jovevska <tam.jov@gmail.com>
2016-05-09 20:26:44 +0200

    Proba

--- KONECHNO/tests/sample/TamaraTest2.java
+++ KONECHNO/tests/sample/TamaraTest2.java
@@ -15,8 +15,7 @@ public class TamaraTest2 {
 		assertEquals(12,Tamara.scalarMultiplication(new int[]{3,2}, new int[]{2,3}));
 		assertEquals(12,Tamara.scalarMultiplication(new int[]{-3,2}, new int[]{-2,3}));
 		assertEquals(20,Tamara.scalarMultiplication(new int[]{4,-2}, new int[]{4,-2}));
-		assertEquals(0,Tamara.scalarMultiplication(new int[]{1,-1}, new int[]{2,2}));
-		assertEquals(2,Tamara.scalarMultiplication(new int[]{-1,-1}, new int[]{-1,-1}));
+		
 		
 		
 	}
